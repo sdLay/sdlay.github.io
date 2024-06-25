9b83aae6faab4c1a5fff7c75d413e6d7f620f59a
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-06-25 15:46:35 +1000

    update static folder

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import requests
 
 baseurl = "https://sdlay.github.io/"
 ext = "user-info/ylay/info.json"
+ext = ".well-known/webfinger?resource=acct:ylay@sdlay.github.io"
 
 url = baseurl + ext 
 res = requests.get(url)
