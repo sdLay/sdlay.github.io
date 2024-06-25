b10190f0a8b88bb671727467142af5a1ce053976
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-06-25 13:36:35 +1000

    update actor info

--- docs/run.py
+++ docs/run.py
@@ -1,8 +1,16 @@
 import requests
 
 url = "http://localhost:4000/user-info/ylay/info"
- 
-response = requests.get(url)
+url = "https://daniellay.cc/.well-known/webfinger?resource=acct:dlay@daniellay.cc"
+
+url = "https://daniellay.cc/u/dlay/followers"
+url = "https://daniellay.cc/u/dlay#key"
+headers = {
+	'accept': 'application/activity+json'
+}
+
+response = requests.get(url, headers=headers)
 
 if response.ok: 
+	# print(response.headrs)
 	print(response.json())
