0eb5b2b43f832a3e899468387b65f63a23f59ba8
Simon Lay <ylay3633@uni.sydney.edu.au>
2024-06-25 13:45:49 +1000

    update webfinger

--- docs/run.py
+++ docs/run.py
@@ -3,8 +3,9 @@ import requests
 url = "http://localhost:4000/user-info/ylay/info"
 url = "https://daniellay.cc/.well-known/webfinger?resource=acct:dlay@daniellay.cc"
 
-url = "https://daniellay.cc/u/dlay/followers"
+# url = "https://daniellay.cc/u/dlay/followers"
 url = "https://daniellay.cc/u/dlay#key"
+url = "https://daniellay.cc/u/dlay"
 headers = {
 	'accept': 'application/activity+json'
 }
@@ -14,3 +15,5 @@ response = requests.get(url, headers=headers)
 if response.ok: 
 	# print(response.headrs)
 	print(response.json())
+else: 
+	print(response.status_code)
